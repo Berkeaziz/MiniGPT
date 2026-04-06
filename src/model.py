e3bf2aa6be4c8991208c406be0a2502a1489d9ce
berkeaziz <berke-aziz2001@hotmail.com>
2026-04-06 17:19:40 +0300

    Enhance generate method in GPTLanguageModel to support temperature and top-k sampling

--- src/model.py
+++ src/model.py
@@ -166,15 +166,22 @@ class GPTLanguageModel(nn.Module):
 
         return logits, loss
 
-    def generate(self, idx, max_new_tokens):
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
         for _ in range(max_new_tokens):
             idx_cond = idx[:, -self.block_size:]
             logits, _ = self(idx_cond)
 
-            logits = logits[:, -1, :]
-            probs = F.softmax(logits, dim=-1)
+            logits = logits[:, -1, :]  
 
+            logits = logits / temperature
+
+            if top_k is not None:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, [-1]]] = float("-inf")
+
+            probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
+
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
